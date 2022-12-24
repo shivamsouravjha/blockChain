55ed736bb5dd3432460f2b19f9e9897840043731
shivamsouravjha <shivam.jha@smallcase.com>
2022-12-24 13:19:07 +0530

    fixing mongodb connection

--- index.js
+++ index.js
@@ -33,13 +33,11 @@ app.use((error, req, res, next) => {          //special 4 term function that let
 
 mongoose
     .connect(
-      `mongodb://${process.env.name}:${process.env.password}@cluster0-shard-00-00.dm1xw.mongodb.net:27017,cluster0-shard-00-01.dm1xw.mongodb.net:27017,cluster0-shard-00-02.dm1xw.mongodb.net:27017/${process.env.db}?ssl=true&replicaSet=atlas-x6eag6-shard-0&authSource=admin&retryWrites=true&w=majority`,
-      { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true }
-      
+      process.env.mongodb
     )
     .then(() => {
-      console.log('listening at port',process.env.PORT || 5001 )
-      app.listen(process.env.PORT || 5001);
+      console.log('listening at port',process.env.PORT || 5002 )
+      app.listen(process.env.PORT || 5002);
     })
     .catch(err => {
       console.log(err);
